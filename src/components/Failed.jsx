4419b217e428a3965951c9fa5563708783a49dfe
Your Name <your.email@example.com>
2025-08-15 10:11:18 +0500

    Project-finished

--- src/components/Failed.jsx
+++ src/components/Failed.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import LanguageSelect from '../LanguageSelect';
+import './failed.css'
 const Failed = () => {
+  const navigate = useNavigate();
+
+  const handleBackHome = () => {
+    navigate('/'); // bosh sahifaga qaytish
+  };
+
   return (
-    <div>
-      <h1>Failed</h1>
+    <div className='insurance-container'>
+      <div className="header">
+        <div className="logo">
+          <img width={68} src="/logo.png" alt="" />
+          <img width={72} src="/location.png" alt="" />
+        </div>
+
+        <div className="lang">
+          <a href="tel: +998936993668">
+            <img width={32} src="/call.png" alt="" />
+          </a>
+          <LanguageSelect />
+        </div>
+      </div>
+
+      <div className="failed-info">
+        <h1 style={{
+          fontSize: '26px',
+          fontFamily: 'sans-serif',
+          textAlign: 'center',
+          color: 'white'
+        }}>Your payment
+          <br />failed!</h1>
+
+        <div className="failed-modal">
+          <img width={43} height={43} src="/error.png" alt="" />
+          <h2 style={{
+            fontSize: '16px',
+            fontFamily: 'sans-serif',
+            textAlign: 'center',
+          }}>Unfortunately, an error is occurred!</h2>
+          <p style={{
+            fontSize: '14px',
+            fontFamily: 'sans-serif',
+            textAlign: 'center',
+            color: 'gray'
+          }}>Possible reasons:</p>
+          <ul class="custom-list">
+            <li>There are not enough funds on the card</li>
+            <li>The card limit is exceeded</li>
+            <li>Card details entered incorrectly</li>
+            <li>The card is expired</li>
+            <li>The card cannot be used for online payments</li>
+            <li>
+              The 3D Secure service is not activated
+              (an SMS with an one-time code is sent to your phone to confirm the operation)
+            </li>
+          </ul>
+                    <p style={{
+            fontSize: '14px',
+            fontFamily: 'sans-serif',
+            textAlign: 'center',
+            color: 'gray'
+          }}>Please contact your support service and we will provide alternative payment variants.</p>
+
+        </div>
+        <button className='back-btn' onClick={handleBackHome}>
+          Return to Home
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LanguageSelect from '../LanguageSelect';
+import './success.css'
+const Failed = () => {
+  const navigate = useNavigate();
 
-const Success = () => {
+  const handleBackHome = () => {
+    navigate('/'); // bosh sahifaga qaytish
+  };
+
   return (
-    <div>
-      <h1>Success</h1>
+    <div className='insurance-container'>
+      <div className="header">
+        <div className="logo">
+          <img width={68} src="/logo.png" alt="" />
+          <img width={72} src="/location.png" alt="" />
+        </div>
+
+        <div className="lang">
+          <a href="tel: +998936993668">
+            <img width={32} src="/call.png" alt="" />
+          </a>
+          <LanguageSelect />
+        </div>
+      </div>
+
+      <div className="success-info">
+        <h1 style={{
+          fontSize: '26px',
+          fontFamily: 'sans-serif',
+          textAlign: 'center',
+          color: 'white'
+        }}>Your payment is successful!</h1>
+
+        <div className="success-modal">
+          <img width={43} height={43} src="/dbl.png" alt="" />
+          <h2 style={{
+            fontSize: '16px',
+            fontFamily: 'sans-serif',
+            textAlign: 'center',
+          }}>Thank you for choosing our service!</h2>
+          <p style={{
+            fontSize: '14px',
+            fontFamily: 'sans-serif',
+            textAlign: 'center',
+            color: 'gray'
+          }}>All documents will be sent to your
+            e-mail within 30 minutes!
+            If the letter is not received,
+            you can contact our support team</p>
+        </div>
+        <button className='back-btn' onClick={handleBackHome}>
+          Return to Home
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Success;
+export default Failed;
